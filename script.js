8dc1a5ff07140d00cbb3c17390b73f01fd6ef638
benjamingolder <84680012+benjamingolder@users.noreply.github.com>
2024-08-29 18:04:42 +0200

    Update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const fruitPieces = function(fruit){
+    return fruit;
+}
+
+const fruitProcessor = function(apples, organges){
+    const applePieces = fruitPieces(apples);
+    const organgePieces = fruitPieces(organges);
+    const juice = `Juice with ${apples} apples and ${organges} organges`;
+    return juice;
+}
+
+let applesInput = prompt("How many apples?");
+let organgesInput = promt("How many oranges");
+console.log(fruitProcessor(applesInput,organgesInput));
+
+
